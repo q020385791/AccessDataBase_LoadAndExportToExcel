06f1ea269335d8ef62620a530af4ac3a858de0f5
sai wanza <q020385791@gmail.com>
2020-02-04 17:01:03 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace AccCount
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtSelectByText = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.txtQuery = new System.Windows.Forms.TextBox();
+            this.btnexport = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGetFile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSelectByText
+            // 
+            this.txtSelectByText.Location = new System.Drawing.Point(183, 450);
+            this.txtSelectByText.Name = "txtSelectByText";
+            this.txtSelectByText.Size = new System.Drawing.Size(149, 63);
+            this.txtSelectByText.TabIndex = 0;
+            this.txtSelectByText.Text = "SQL命令Fire";
+            this.txtSelectByText.UseVisualStyleBackColor = true;
+            this.txtSelectByText.Click += new System.EventHandler(this.TxtSelectByText_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 31;
+            this.dataGridView1.Size = new System.Drawing.Size(750, 144);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // txtQuery
+            // 
+            this.txtQuery.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtQuery.Location = new System.Drawing.Point(12, 175);
+            this.txtQuery.Multiline = true;
+            this.txtQuery.Name = "txtQuery";
+            this.txtQuery.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtQuery.Size = new System.Drawing.Size(1038, 269);
+            this.txtQuery.TabIndex = 3;
+            // 
+            // btnexport
+            // 
+            this.btnexport.Location = new System.Drawing.Point(338, 450);
+            this.btnexport.Name = "btnexport";
+            this.btnexport.Size = new System.Drawing.Size(125, 63);
+            this.btnexport.TabIndex = 4;
+            this.btnexport.Text = "匯出至Excel";
+            this.btnexport.UseVisualStyleBackColor = true;
+            this.btnexport.Click += new System.EventHandler(this.Btnexport_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 18);
+            this.label1.TabIndex = 5;
+            // 
+            // btnGetFile
+            // 
+            this.btnGetFile.Location = new System.Drawing.Point(12, 450);
+            this.btnGetFile.Name = "btnGetFile";
+            this.btnGetFile.Size = new System.Drawing.Size(165, 63);
+            this.btnGetFile.TabIndex = 6;
+            this.btnGetFile.Text = "選擇檔案";
+            this.btnGetFile.UseVisualStyleBackColor = true;
+            this.btnGetFile.Click += new System.EventHandler(this.BtnGetFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1097, 515);
+            this.Controls.Add(this.btnGetFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnexport);
+            this.Controls.Add(this.txtQuery);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtSelectByText);
+            this.Name = "Form1";
+            this.Text = "AccCount";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button txtSelectByText;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.TextBox txtQuery;
+        private System.Windows.Forms.Button btnexport;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGetFile;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+namespace AccCount
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void TxtSelectByText_Click(object sender, EventArgs e)
+        {
+            OleDbConnection conn = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source="+ label1.Text + "");
+            OleDbCommand cmd = new OleDbCommand();
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.StoredProcedure;
+            var sqlquery = "";
+            try
+            {
+                sqlquery = txtQuery.Text;
+                OleDbDataAdapter Adapter = new OleDbDataAdapter();
+                DataSet dataSet = new DataSet();
+                //OleDbCommand cmd = new OleDbCommand(sql, connection);
+                Adapter.SelectCommand = new OleDbCommand(sqlquery, conn);
+                Adapter.Fill(dataSet);
+                dataGridView1.DataSource = dataSet.Tables[0];
+                dataGridView1.Refresh();
+                dataGridView1.Columns[0].Selected = true;
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Btnexport_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            save.FileName = "Excel Export demo";
+            save.Filter = "*.xlsx|*.xlsx";
+            if (save.ShowDialog() != DialogResult.OK) return;
+
+            // Excel 物件
+            Excel.Application xls = null;
+            try
+            {
+                xls = new Excel.Application();
+                // Excel WorkBook
+                Excel.Workbook book = xls.Workbooks.Add();
+                Excel.Worksheet Sheet = xls.ActiveSheet;
+                // 把 DataGridView 資料塞進 Excel 內
+                DataGridView2Excel(Sheet);
+                // 儲存檔案
+                book.SaveAs(save.FileName);
+                MessageBox.Show("轉換完成");
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                xls.Quit();
+            }
+        }
+        private void DataGridView2Excel(Excel.Worksheet Sheet)
+        {
+            var Rows=dataGridView1.Rows.Count;
+            var ColumnCount = dataGridView1.Columns.Count;
+
+            for (int i = 0; i < dataGridView1.Rows.Count-1; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    try
+                    {
+
+                        if (!string.IsNullOrEmpty(dataGridView1[j, i].Value.ToString()))
+                        {
+                           
+                            Sheet.Cells[i +2, j+1] = dataGridView1[j, i].Value.ToString();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+            //插入標頭
+            for (int i = 0; i < ColumnCount; i++)
+            {
+                Sheet.Cells[1, i + 1] = dataGridView1.Columns[i].HeaderText;
+            }
+        }
+        private void BtnGetFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog flog = new OpenFileDialog();
+            flog.Filter = "All Files (*.*)|*.*";
+            flog.FilterIndex = 1;
+            flog.Multiselect = true;
+            if (flog.ShowDialog() == DialogResult.OK)
+            {
+                string sFileName = flog.FileName;
+                string[] arrAllFiles = flog.FileNames; //used when Multiselect = true       
+                label1.Text = sFileName;
+            }
+        }
+    }
+}
